9d9e57cd31816108996526e105e5f869aee4ca00
Miguel Vásquez <angelzizu06@gmail.com>
2024-08-29 10:28:33 -0500

    Se crea la estructura inicial del proyecto

---
+++ src/app/index.jsx
@@ -0,0 +1,12 @@
+import { Board } from "../components/Board";
+import styles from "./styles.module.scss";
+
+function App() {
+  return (
+    <main className={styles.Main}>
+      <Board />
+    </main>
+  );
+}
+
+export default App;
