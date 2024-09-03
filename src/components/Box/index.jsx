e66d3cf25cea8b2ed7a2f38e7783cf8aafd30872
Miguel Vásquez <angelzizu06@gmail.com>
2024-09-02 23:25:46 -0500

    Se separan las funciones en handles

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -4,31 +4,56 @@ import { AppContext } from "../../context/appContext";
 import {
   CHANGE_PLAYER,
   EDIT_BOXES,
+  FINISH_GAME,
   VALIDATE_WINNER,
 } from "../../reducer/types";
 import Equix from "../../assets/equix.png";
 import Circle from "../../assets/circle.png";
+import { useValidateGame } from "../../hooks/useValidateGame";
+import { getNewBoxes } from "../../utils/getNewBoxes";
+import { toValidateFinish } from "../../utils/toValidateFinish";
 
 const Box = ({ value = "", coordinates }) => {
   const [store, dispatch] = useContext(AppContext);
-
+  const { handleValidate } = useValidateGame();
   const icons = {
-    X: Equix,
-    O: Circle,
+    "#": Equix,
+    "&": Circle,
   };
 
-  const handleClick = () => {
-    const character = store.player === 1 ? "X" : "O";
-
+  const handlePlay = async () => {
+    const character = store.player === 1 ? "#" : "&";
+    const newBoxes = getNewBoxes({
+      coordinates,
+      value: character,
+      boxes: store.boxes,
+    });
     dispatch({
       type: EDIT_BOXES,
-      payload: { coordinates: coordinates, value: character },
+      payload: newBoxes,
     });
-    dispatch({ type: VALIDATE_WINNER });
-    dispatch({
-      type: CHANGE_PLAYER,
-      payload: store.player === 1 ? 2 : 1,
+    return newBoxes;
+  };
+
+  const handleChangePlayer = (nextPlayer) => {
+    dispatch({ type: CHANGE_PLAYER, payload: nextPlayer });
+  };
+
+  const handleFinish = () => {
+    dispatch({ type: FINISH_GAME, payload: store.player });
+  };
+
+  const handleClick = async () => {
+    const nextPlayer = store.player === 1 ? 2 : 1;
+    const { isWinner, isFinish } = await handlePlay().then((newBoxes) => {
+      const isWinner = handleValidate(newBoxes);
+      const isFinish = toValidateFinish(newBoxes);
+      return { isWinner, isFinish };
     });
+    dispatch({ type: VALIDATE_WINNER, payload: isWinner });
+
+    if (isFinish || isWinner) return handleFinish();
+    handleChangePlayer(nextPlayer);
   };
 
   return (
