91cca8b3fbb05815285959fb55622525c382a360
Miguel Vásquez <angelzizu06@gmail.com>
2024-09-02 23:23:20 -0500

    Se crea un custom hook para validar al ganador

---
+++ src/hooks/useValidateGame.jsx
@@ -0,0 +1,42 @@
+import {
+  getPrincipalDiagonal,
+  getSecondaryDiagonal,
+} from "../utils/getDiagonals";
+import { transposedMatrix } from "../utils/trasposedMatrix";
+
+const useValidateGame = () => {
+  const winnerCases = ["###", "&&&"];
+  let isWinner = false;
+  const handleValidate = (boxes) => {
+    const boxesTrasposed = transposedMatrix(boxes);
+    const principalDiagonal = getPrincipalDiagonal(boxes);
+    const secondaryDiagonal = getSecondaryDiagonal(boxes);
+
+    // case 1
+    boxes.forEach((column) => {
+      if (winnerCases.includes(column.join(""))) {
+        isWinner = true;
+      }
+    });
+    // case 2
+    boxesTrasposed.forEach((column) => {
+      if (winnerCases.includes(column.join(""))) {
+        isWinner = true;
+      }
+    });
+    // case 3
+    if (winnerCases.includes(principalDiagonal.join(""))) {
+      isWinner = true;
+    }
+    // case 4
+    if (winnerCases.includes(secondaryDiagonal.join(""))) {
+      isWinner = true;
+    }
+
+    return isWinner;
+  };
+
+  return { handleValidate };
+};
+
+export { useValidateGame };
